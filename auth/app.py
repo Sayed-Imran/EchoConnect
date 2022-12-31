44f83df3017c883b3d2fc8138956cff1f66da1e0
PIIOT22NOV3006 <sayedimran00786@gmail.com>
2022-12-31 19:36:12 +0530

    updated imports

--- auth/app.py
+++ auth/app.py
@@ -1,11 +1,11 @@
+from dotenv import load_dotenv
+
+load_dotenv()
 from scripts.config import SERVERConf
 
 import uvicorn
 
 
 if __name__ == "__main__":
-    from dotenv import load_dotenv
-
-    load_dotenv()
 
     uvicorn.run("main:app", host=SERVERConf.host, port=SERVERConf.port, reload=True)
