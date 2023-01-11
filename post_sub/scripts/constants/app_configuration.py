e427e5ae04f281a786cc12e0b4285a3a1ad1a92e
PIIOT22NOV3006 <sayedimran00786@gmail.com>
2023-01-11 20:23:39 +0530

    Debugging

--- post_sub/scripts/constants/app_configuration.py
+++ post_sub/scripts/constants/app_configuration.py
@@ -39,6 +39,7 @@ class DBConf:
 
     class GoogleCred:
         cred = config.get("GOOGLE_APPLICATION_CREDENTIALS", "GOOGLE_CRED")
+        print(cred)
         if not cred:
             print("Error, ENV variable Google Cred not set")
             sys.exit(1)
