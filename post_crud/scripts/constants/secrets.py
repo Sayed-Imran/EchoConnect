bbaa9db6f33460601a076674fe29b34be0d2d757
PIIOT22NOV3006 <sayedimran00786@gmail.com>
2023-03-12 14:10:01 +0530

    Changing JWT expire time

--- post_crud/scripts/constants/secrets.py
+++ post_crud/scripts/constants/secrets.py
@@ -2,6 +2,6 @@ from scripts.config import AuthenticationConf
 
 
 class Secrets:
-    LOCK_OUT_TIME_MINS = 60
+    LOCK_OUT_TIME_MINS = 525600
     UNIQUE_KEY = AuthenticationConf.encryption_key
     ALG = "HS256"
